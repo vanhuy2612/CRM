8ce1248cfd4050b41d680bf4c8fecc7c5bd06f72
unknown <vanhuy2612@gmail.com>
2020-04-18 00:29:06 +0700

    Khoi tao server node

---
+++ backend/config/dbConfig.js
@@ -0,0 +1,10 @@
+const {DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASS, DB_LOC} = process.env
+const conn = require('mssql')
+
+
+module.exports = {
+    user: DB_USER || 'root',
+    password: DB_PASS || '',
+    server: DB_HOST || 'localhost',
+    database: DB_NAME || ''
+}
