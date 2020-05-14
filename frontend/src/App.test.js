69210898b738ae40acde60f0640c2e4d87c9a4de
dotienduong <do.tien.duong@mqsolutions.com.vn>
2020-05-14 13:54:40 +0700

    test

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -3,6 +3,7 @@ import { render } from '@testing-library/react';
 import Main from './Views/Login';
 
 test('renders learn react link', () => {
+console.log('test')
   const { getByText } = render(<Main />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
