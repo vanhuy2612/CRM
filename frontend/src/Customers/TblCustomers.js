464b0d4912b1ecb071df53f7bcb4344ed1dda80d
dotienduong <do.tien.duong@mqsolutions.com.vn>
2020-05-14 17:39:06 +0700

    update tblOrder and tblCustomer

--- frontend/src/Customers/TblCustomers.js
+++ frontend/src/Customers/TblCustomers.js
@@ -5,12 +5,12 @@ class TblCustomers extends Component {
     constructor(props) {
         super(props)
         this.columns = [
-            { title: 'Name', field: 'name' },
-            { title: 'Address', field: 'address' },
-            { title: 'Phone', field: 'phone' },
-            { title: 'Birth Year', field: 'dateOfBirth', type: 'numeric' },
-            { title: 'Type', field: 'type' },
-            { title: 'BranchId', field: 'branchId' },
+            { title: 'Name', field: 'name', type: "String" },
+            { title: 'Address', field: 'address', type: "String" },
+            { title: 'Phone', field: 'phone', type: "String" },
+            { title: 'Birth Year', field: 'dateOfBirth', type: 'Date' },
+            { title: 'Type', field: 'type', type: "String" },
+            { title: 'BranchId', field: 'branchId', type: "String" },
         ]
     }
 
@@ -23,43 +23,43 @@ class TblCustomers extends Component {
                 title="Danh sách các khách hàng "
                 columns={columns}
                 data={data}
-                editable={{
-                    onRowAdd: (newData) =>
-                        new Promise((resolve) => {
-                            setTimeout(() => {
-                                resolve();
-                                this.setState((prevState) => {
-                                    const data = [...prevState.data];
-                                    data.push(newData);
-                                    return { ...prevState, data };
-                                });
-                            }, 600);
-                        }),
-                    onRowUpdate: (newData, oldData) =>
-                        new Promise((resolve) => {
-                            setTimeout(() => {
-                                resolve();
-                                if (oldData) {
-                                    this.setState((prevState) => {
-                                        const data = [...prevState.data];
-                                        data[data.indexOf(oldData)] = newData;
-                                        return { ...prevState, data };
-                                    });
-                                }
-                            }, 600);
-                        }),
-                    onRowDelete: (oldData) =>
-                        new Promise((resolve) => {
-                            setTimeout(() => {
-                                resolve();
-                                this.setState((prevState) => {
-                                    const data = [...prevState.data];
-                                    data.splice(data.indexOf(oldData), 1);
-                                    return { ...prevState, data };
-                                });
-                            }, 600);
-                        }),
-                }}
+                // editable={{
+                //     onRowAdd: (newData) =>
+                //         new Promise((resolve) => {
+                //             setTimeout(() => {
+                //                 resolve();
+                //                 this.setState((prevState) => {
+                //                     const data = [...prevState.data];
+                //                     data.push(newData);
+                //                     return { ...prevState, data };
+                //                 });
+                //             }, 600);
+                //         }),
+                //     onRowUpdate: (newData, oldData) =>
+                //         new Promise((resolve) => {
+                //             setTimeout(() => {
+                //                 resolve();
+                //                 if (oldData) {
+                //                     this.setState((prevState) => {
+                //                         const data = [...prevState.data];
+                //                         data[data.indexOf(oldData)] = newData;
+                //                         return { ...prevState, data };
+                //                     });
+                //                 }
+                //             }, 600);
+                //         }),
+                //     onRowDelete: (oldData) =>
+                //         new Promise((resolve) => {
+                //             setTimeout(() => {
+                //                 resolve();
+                //                 this.setState((prevState) => {
+                //                     const data = [...prevState.data];
+                //                     data.splice(data.indexOf(oldData), 1);
+                //                     return { ...prevState, data };
+                //                 });
+                //             }, 600);
+                //         }),
+                // }}
             />
         );
     }
--- frontend/src/Orders/TblOrders.js
+++ frontend/src/Orders/TblOrders.js
@@ -1,63 +1,63 @@
 import React, { Component } from 'react';
 import MaterialTable from 'material-table';
 
-class TblOrders extends Component{
-    constructor(props){
+class TblOrders extends Component {
+    constructor(props) {
         super(props)
         this.columns = [
-                { title: 'Id', field: 'id' },
-                { title: 'Price', field: 'price', type: 'number' },
-                { title: 'Quantity', field: 'quantity', type: 'number' },
-                { title: 'ItemId', field: 'itemId' },
-                { title: 'CustomerId', field: 'customerId' },
-                // { title: 'CardId', field: 'cardId' },
-            ]
+            { title: 'Id', field: 'id', type: "String" },
+            { title: 'Price', field: 'price', type: 'Float' },
+            { title: 'Quantity', field: 'quantity', type: 'Interger' },
+            { title: 'ItemId', field: 'itemId', type: "String" },
+            { title: 'CustomerId', field: 'customerId', type: "String" },
+            // { title: 'CardId', field: 'cardId', type: "String" },
+        ]
     }
-    render(){
-        const {columns} = this
+    render() {
+        const { columns } = this
         const { data } = this.props
         return (
             <MaterialTable
                 title="Danh sách đặt hàng"
                 columns={columns}
                 data={data}
-                editable={{
-                    onRowAdd: (newData) =>
-                        new Promise((resolve) => {
-                            setTimeout(() => {
-                                resolve();
-                                this.setState((prevState) => {
-                                    const data = [...prevState.data];
-                                    data.push(newData);
-                                    return { ...prevState, data };
-                                });
-                            }, 600);
-                        }),
-                    onRowUpdate: (newData, oldData) =>
-                        new Promise((resolve) => {
-                            setTimeout(() => {
-                                resolve();
-                                if (oldData) {
-                                    this.setState((prevState) => {
-                                        const data = [...prevState.data];
-                                        data[data.indexOf(oldData)] = newData;
-                                        return { ...prevState, data };
-                                    });
-                                }
-                            }, 600);
-                        }),
-                    onRowDelete: (oldData) =>
-                        new Promise((resolve) => {
-                            setTimeout(() => {
-                                resolve();
-                                this.setState((prevState) => {
-                                    const data = [...prevState.data];
-                                    data.splice(data.indexOf(oldData), 1);
-                                    return { ...prevState, data };
-                                });
-                            }, 600);
-                        }),
-                }}
+                // editable={{
+                //     onRowAdd: (newData) =>
+                //         new Promise((resolve) => {
+                //             setTimeout(() => {
+                //                 resolve();
+                //                 this.setState((prevState) => {
+                //                     const data = [...prevState.data];
+                //                     data.push(newData);
+                //                     return { ...prevState, data };
+                //                 });
+                //             }, 600);
+                //         }),
+                //     onRowUpdate: (newData, oldData) =>
+                //         new Promise((resolve) => {
+                //             setTimeout(() => {
+                //                 resolve();
+                //                 if (oldData) {
+                //                     this.setState((prevState) => {
+                //                         const data = [...prevState.data];
+                //                         data[data.indexOf(oldData)] = newData;
+                //                         return { ...prevState, data };
+                //                     });
+                //                 }
+                //             }, 600);
+                //         }),
+                //     onRowDelete: (oldData) =>
+                //         new Promise((resolve) => {
+                //             setTimeout(() => {
+                //                 resolve();
+                //                 this.setState((prevState) => {
+                //                     const data = [...prevState.data];
+                //                     data.splice(data.indexOf(oldData), 1);
+                //                     return { ...prevState, data };
+                //                 });
+                //             }, 600);
+                //         }),
+                // }}
             />
         );
     }
