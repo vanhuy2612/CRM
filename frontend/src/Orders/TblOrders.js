b05bcc363bfa87ef152be9505f99a23874dda4a6
dotienduong <do.tien.duong@mqsolutions.com.vn>
2020-05-14 17:28:22 +0700

    update order Item ( chưa có add order )

--- frontend/src/Orders/TblOrders.js
+++ frontend/src/Orders/TblOrders.js
@@ -4,25 +4,18 @@ import MaterialTable from 'material-table';
 class TblOrders extends Component{
     constructor(props){
         super(props)
-        this.state={
-            columns: [
+        this.columns = [
                 { title: 'Id', field: 'id' },
                 { title: 'Price', field: 'price', type: 'number' },
                 { title: 'Quantity', field: 'quantity', type: 'number' },
                 { title: 'ItemId', field: 'itemId' },
-                { title: 'CustomerId', field: 'customerId ' },
-                { title: 'CardId', field: 'cardId' },
-            ],
-            data: [
-                { id: "dagdbbvaq874361ajvh8", price: '100000', quantity: '10', itemId: "jdagsdg1287fh", customerId: 'dagdbbvaq874361a', cardId: 'sfsdf' },
-                { id: "dagdbbq7crt398r61ajvh8", price: '100000', quantity: '8', itemId: "hadf761esahd", customerId: 'dagdbbvaq874361a', cardId: 'sfsd' },
-                { id: "dagdbr87613ru1ajvh8", price: '100000', quantity: '7', itemId: "qadtx712e6hd", customerId: 'dagdbbvaq874361a', cardId: 'sdfsd' },
-                { id: "dagdbbvaq2784cf8y4ajvh8", price: '100000', quantity: '6', itemId: "xd1edxad8", customerId: 'dagdbbvaq874361a', cardId: 'sfsd' },
-            ],
-        }
+                { title: 'CustomerId', field: 'customerId' },
+                // { title: 'CardId', field: 'cardId' },
+            ]
     }
     render(){
-        const {columns, data} = this.state
+        const {columns} = this
+        const { data } = this.props
         return (
             <MaterialTable
                 title="Danh sách đặt hàng"
