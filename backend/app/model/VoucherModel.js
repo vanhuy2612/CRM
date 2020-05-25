a5618be5274a1666ee2657f678ea86368b578a4c
vanhuy2612 <vanhuy2612@gmail.com>
2020-05-25 23:04:48 +0700

    update voucher

--- backend/app/model/VoucherModel.js
+++ backend/app/model/VoucherModel.js
@@ -2,6 +2,7 @@
 
 module.exports = (sequelize, DataTypes) => {
     const VoucherModel = sequelize.define('vouchers', {
+        id: {type: DataTypes.STRING, primaryKey: true},
         customerId: {type: DataTypes.STRING},
         type: {type: DataTypes.STRING},
         discount: {type: DataTypes.FLOAT}
