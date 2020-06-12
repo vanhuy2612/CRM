8598690f135f059a68ffb0866511a59591773ff6
DTDUONG98 <dotienduongptit@gmail.com>
2020-06-12 21:29:20 +0700

    update code chart Deal

--- frontend/src/Deals/TblDeals.js
+++ frontend/src/Deals/TblDeals.js
@@ -1,41 +1,25 @@
 import React, { Component } from 'react';
 import 'antd/dist/antd.css';
-import { Upload, Button, Icon } from 'antd';
-import { UploadOutlined } from '@ant-design/icons';
+import { Upload, Button } from 'antd';
+import CanvasJSReact from './canvasjs.react';
+var CanvasJS = CanvasJSReact.CanvasJS;
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 class image extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      fileList : []
+      fileList: []
     }
   }
-  handleChange = (info) => {
-    let fileList = info.fileList;
-
-    // 1. Limit the number of uploaded files
-    //    Only to show two recent uploaded files, and old ones will be replaced by the new
-    fileList = fileList.slice(-2);
-
-    // 2. read from response and show file link
-    fileList = fileList.map((file) => {
-      if (file.response) {
-        // Component will show file.url as link
-        file.url = file.response.url;
-        console.log('url image', file )
-      }
-      return file;
-    });
-
-    // 3. filter successfully uploaded files according to response from server
-    fileList = fileList.filter((file) => {
-      if (file.response) {
-        return file.response.status === 'success';
-      }
-      return true;
-    });
-
-    this.setState({ fileList });
-  }
+  toggleDataSeries = (e) => {
+		if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+			e.dataSeries.visible = false;
+		}
+		else{
+			e.dataSeries.visible = true;
+		}
+		this.chart.render();
+	}
   render() {
     const props = {
       action: '//jsonplaceholder.typicode.com/posts/',
@@ -43,13 +27,85 @@ class image extends Component {
       defaultFileList: [...this.state.fileList],
       onChange: this.handleChange,
     }
+    const options = {
+			animationEnabled: true,
+			title:{
+				text: "All Time Summer Olympic Medals"
+			},
+			legend: {
+				verticalAlign: "center",
+				horizontalAlign: "right",
+				reversed: true,
+				cursor: "pointer",
+					fontSize: 16,
+					itemclick: this.toggleDataSeries
+			},
+			toolTip: {
+				shared: true
+			},
+			data: [
+			{
+				type: "stackedColumn100",
+				name: "Gold",
+				showInLegend: true,
+				color: "#D4AF37",
+				dataPoints: [
+					{ label: "United States", y:1118},
+					{ label: "Soviet Union", y:473},
+					{ label: "Great Britain", y:273},
+					{ label: "France", y:243},
+					{ label: "Germany", y:269},
+					{ label: "Italy", y:243},
+					{ label: "Sweden", y:195},
+					{ label: "China", y:236},
+					{ label: "Russia", y:194},
+					{ label: "East Germany", y:192}
+				]
+			},
+			{
+				type: "stackedColumn100",
+				name: "Silver",
+				showInLegend: true,
+				color: "#C0C0C0",
+				dataPoints: [
+					{ label: "United States",	y: 897},
+					{ label: "Soviet Union", y: 376},
+					{ label: "Great Britain", y: 299},
+					{ label: "France", y: 272},
+					{ label: "Germany", y: 272},
+					{ label: "Italy",	y: 212},
+					{ label: "Sweden", y: 210},
+					{ label: "China",	y: 189},
+					{ label: "Russia", y: 156},
+					{ label: "East Germany", y: 165}
+				]
+			},
+			{
+				type: "stackedColumn100",
+				name: "Bronze",
+				showInLegend: true,
+				color: "#CD7F32",
+				dataPoints: [
+					{ label: "United States", y: 789},
+					{ label: "Soviet Union", y: 355},
+					{ label: "Great Britain", y: 303},
+					{ label: "France", y: 310},
+					{ label: "Germany",	y: 283},
+					{ label: "Italy", y: 236},
+					{ label: "Sweden", y: 233},
+					{ label: "China", y: 174},
+					{ label: "Russia", y: 187},
+					{ label: "East Germany", y: 162}
+				]
+			}
+			]
+		}
+    const { pass, repass } = this.state
     return (
       <div>
-        <Upload {...props}>
-          <Button>
-            <UploadOutlined /> upload
-      </Button>
-        </Upload>
+        <CanvasJSChart options = {options}
+				 onRef={ref => this.chart = ref}
+			/>
       </div>
     )
   }
